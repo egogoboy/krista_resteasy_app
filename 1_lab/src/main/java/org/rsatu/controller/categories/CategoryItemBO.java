47769c6bb1d86c4c2fb9f19165428630e5a785ad
egogoboy <egor2005sudakov@gmail.com>
2025-10-23 11:17:13 +0300

    Added BO classes

--- 1_lab/src/main/java/org/rsatu/controller/categories/CategoryItemBO.java
+++ 1_lab/src/main/java/org/rsatu/controller/categories/CategoryItemBO.java
@@ -1,5 +1,27 @@
 package org.rsatu.controller.categories;
 
 public class CategoryItemBO {
+    private Long id;
+    private String title;
 
+    public CategoryItemBO(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String new_title) {
+        this.title = new_title;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
 }
--- 1_lab/src/main/java/org/rsatu/controller/news/NewsItemBO.java
+++ 1_lab/src/main/java/org/rsatu/controller/news/NewsItemBO.java
@@ -1,5 +1,90 @@
 package org.rsatu.controller.news;
 
-public class NewsItemBO {
+import java.util.Date;
 
+public class NewsItemBO {
+    private Long id;
+    private String title;
+    private String text;
+    private String image;
+    private String category;
+    private Date publish_date;
+    private Integer likes;
+
+    public NewsItemBO(
+            Long id,
+            String title,
+            String text,
+            String image,
+            String category,
+            Date publish_date,
+            Integer likes) {
+        this.id = id;
+        this.title = title;
+        this.text = text;
+        this.image = image;
+        this.category = category;
+        this.publish_date = publish_date;
+        this.likes = likes;
+    }
+
+    public void increaseLikes(Integer count) {
+        this.likes += count;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String new_title) {
+        this.title = new_title;
+    }
+
+    public void setText(String new_text) {
+        this.text = new_text;
+    }
+
+    public void setImage(String new_image) {
+        this.image = new_image;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setPublishDate(Date date) {
+        this.publish_date = date;
+    }
+
+    public void setLikes(Integer likes_count) {
+        this.likes = likes_count;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public String getImage() {
+        return this.image;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public Date getPublishDate() {
+        return this.publish_date;
+    }
+
+    public Integer getLikesCount() {
+        return this.likes;
+    }
 }
